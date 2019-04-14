c2c949afaa9af84e59ae68d5b3e8461d2b811c71
Simon Ndunda <simon.ndunda@toptal.com>
2019-04-14 11:23:02 +0300

    Initial commit

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            Hi there guys. Whats up!!! Let's do this. Works!!
           </p>
           <a
             className="App-link"
